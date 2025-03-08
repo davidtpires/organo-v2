8e63cefd2578e2030f4fe10a27b4e01507b7f7fa
David T. <davidt.info@gmail.com>
2025-03-08 17:24:59 -0300

    Criando favorito

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,7 +1,17 @@
 import { IoIosCloseCircle } from "react-icons/io";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 import './colaborador.css'
 
-const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar }) => {
+    function favoritar() {
+        aoFavoritar(colaborador.id)
+    }
+
+    const propsfavorito = {
+        size: 25,
+        onClick: favoritar
+    }
+
     return (<div className="colaborador">
         <IoIosCloseCircle 
             size={25} 
@@ -14,6 +24,12 @@ const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
         <div className="rodape">
             <h4>{colaborador.nome}</h4>
             <h5>{colaborador.cargo}</h5>
+            <div className="favoritar">
+                {colaborador.favorito 
+                    ? <FaHeart {...propsfavorito} color="#ff0000"/> 
+                    : <FaRegHeart {...propsfavorito}/>
+                }
+            </div>
         </div>
     </div>)
 }
